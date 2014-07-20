6d20833124f2076671f4d408631bb8ed2d2fc1ac
tharris29 <th_29@hotmail.co.uk>
2014-07-20 19:38:24 +0100

    Add zip controller and remove standard files

---
+++ API/Controllers/ZipController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+
+namespace API.Controllers
+{
+    public class ZipController : ApiController
+    {
+        public HttpResponseMessage Post()
+        {
+            //todo allow upload and zip
+            HttpResponseMessage result = new HttpResponseMessage(HttpStatusCode.OK);
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue("application/octet-stream");
+            return result;
+        } 
+    }
+}
