3681c41a4fbcb9a9b834491982df6958320bccf5
tharris29 <th_29@hotmail.co.uk>
2014-07-20 14:43:44 +0100

    Initial commit

---
+++ zip-it-api/Controllers/ZipController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace zip_it_api.Controllers
+{
+    public class ZipController : ApiController
+    {
+        // Post api/zip
+        public IEnumerable<string> Post()
+        {//todo actually zip somthing with params
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
---
+++ zip-it-api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="zip_it_api.WebApiApplication" Language="C#" %>
